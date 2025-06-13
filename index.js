605ed00de1a966cfd50814c1a51f6b43248b45c1
martirale <hola@alemartir.com>
2025-06-13 15:33:46 -0600

    Change port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5500;
 
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
